8667c63466d3303a4ba28bc015c7c3c27b4d48db
VinaykUmesh <luxurygeek.umesh@gmail.com>
2021-07-22 11:22:46 +0530

    initializing roles and privileges

---
+++ SpringSecurityWithRolesAndPrivileges/src/main/java/com/spring/security/role/and/privilege/SpringSecurityWithRolesAndPrivilegesApplication.java
@@ -0,0 +1,13 @@
+package com.spring.security.role.and.privilege;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWithRolesAndPrivilegesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityWithRolesAndPrivilegesApplication.class, args);
+	}
+
+}
---
+++ SpringSecurityWithRolesAndPrivileges/src/test/java/com/spring/security/role/and/privilege/SpringSecurityWithRolesAndPrivilegesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.security.role.and.privilege;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityWithRolesAndPrivilegesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
