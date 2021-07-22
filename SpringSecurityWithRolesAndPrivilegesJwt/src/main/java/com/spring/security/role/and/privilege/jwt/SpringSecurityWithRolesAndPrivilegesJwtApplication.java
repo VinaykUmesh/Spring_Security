90e313ab75bffef8365258f1e2420def4b79c1f6
VinaykUmesh <luxurygeek.umesh@gmail.com>
2021-07-22 11:24:44 +0530

    initializing roles and privileges with jwt

---
+++ SpringSecurityWithRolesAndPrivilegesJwt/src/main/java/com/spring/security/role/and/privilege/jwt/SpringSecurityWithRolesAndPrivilegesJwtApplication.java
@@ -0,0 +1,13 @@
+package com.spring.security.role.and.privilege.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWithRolesAndPrivilegesJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityWithRolesAndPrivilegesJwtApplication.class, args);
+	}
+
+}
